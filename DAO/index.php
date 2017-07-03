537c1c20479655d9109f8894ab9e58b50488a3a9
Juka <moura.fagner@gmail.com>
2017-07-03 15:02:47 -0300

    criados os mpetodos de listas

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-
+//carrega um registro
+/*$root = new Usuario();
 $root->loadById(1);
+echo $root;*/
 
-echo $root;
+//carrega uma lista
+/*$lista = Usuario::getlist();
+echo json_encode($lista);*/
+
+//carrega uma lista buscando pelo login
+/*$search = Usuario::search("i");
+echo json_encode($search);*/
+
+//carrega um usuário usando o login e senha
+$usuario = new Usuario();
+$usuario->login("Cris", "32152");
+
+echo $usuario;
 
 ?>
